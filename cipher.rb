c42f6a2b28e85d6bed2233dcc9d579b9d6bfb289
averybrubeck <averybrubeck@yahoo.com>
2023-09-20 20:24:13 -0400

    finished cipher

--- cipher.rb
+++ cipher.rb
@@ -1,7 +1,7 @@
 def ceaser_cipher
     #get input and key
     puts "Enter Password:"
-    pass = gets.chomp 
+    pass = gets.chomp.to_s 
     puts "Enter Encrypt Key:"
     key = gets.chomp.to_i
     while key > 26 do 
@@ -9,10 +9,14 @@ def ceaser_cipher
         key = gets.chomp.to_i
     end
     #convert input to numbers
-
+    new_pass = pass.each_byte.to_a
     #add key to numbers
+    key_pass = new_pass.map { |c| c + key }
+    
     #convert back to string
+    conv_pass = key_pass.pack('c*')
     #log converted string back to console
+    puts conv_pass
 end
 
 ceaser_cipher
