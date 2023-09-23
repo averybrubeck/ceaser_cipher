60d0cd73e54ee8dcf520bbcdf8a5898285ced840
averybrubeck <averybrubeck@yahoo.com>
2023-09-23 16:38:00 -0400

    added decrypt options and allowed more input

--- cipher.rb
+++ cipher.rb
@@ -1,22 +1,49 @@
+require 'pry-byebug'
 def ceaser_cipher
-    #get input and key
+ 
+    puts "Encrypt or Decrypt Password?" 
+    user_choice = gets.chomp.to_s.downcase
+        
+    #binding.pry
+    if user_choice == "encrypt" || user_choice == "e"           
+    #binding.pry
+
     puts "Enter Password:"
-    pass = gets.chomp.to_s 
+    pass = gets.chomp 
+    
     puts "Enter Encrypt Key:"
     key = gets.chomp.to_i
+    
     while key > 26 do 
         puts "Key To Large Re-enter Encrypt Key:"
         key = gets.chomp.to_i
     end
-    #convert input to numbers
-    new_pass = pass.each_byte.to_a
-    #add key to numbers
-    key_pass = new_pass.map { |c| c + key }
     
-    #convert back to string
-    conv_pass = key_pass.pack('c*')
-    #log converted string back to console
-    puts conv_pass
-end
+    e_pass = pass.each_byte.to_a.map { |c| c + key }
+      
+    puts e_pass.pack('c*')
+   
+    #binding.pry
 
+else user_choice == "decrypt" || user_choice == "d"
+    
+    puts "Enter Decryption Key"
+   
+    d_key = gets.chomp.to_i
+    while d_key > 26
+     puts "Enter Decryption Key"
+    d_key = gets.chomp.to_i
+    end
+
+    puts "Enter Password:"
+    pass = gets.chomp
+    
+    d_pass = pass.each_byte.to_a.map { |c| c - d_key}
+
+    puts d_pass.pack('c*')
+    
+
+
+end
+end
 ceaser_cipher
